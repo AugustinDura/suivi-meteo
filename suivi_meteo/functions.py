77cb0613ee71598deb0389b22d02595717e1c487
Augustin Dura <augustindura@gmail.com>
2023-09-07 11:48:59 +0200

    feat: moved  functions to functions folder

--- suivi_meteo/functions.py
+++ suivi_meteo/functions.py
@@ -1,3 +1,21 @@
-## 
-def testfunction():
-    print('hello world')
+
+### 
+
+def url_from_string(date:'string'):
+    # date must be string in the format "YYYY-MM-DD"
+    year = date[0:4] 
+    month = date[5:7]
+    month = int(month)
+    month = str(month-1) # because month index starts at 0
+    day = date[8:10] 
+    if day[0] == "0" :
+        day = day[1:] # in the url single digit days are single digits
+    url = "https://www.meteociel.fr/temps-reel/obs_villes.php?code2=63282001&jour2="+ day + "&mois2="+month+"&annee2="+year
+    return url 
+
+def url_from_datetime(date: datetime):
+    year = str(date.year)
+    month = str(date.month - 1)
+    day = str(date.day)
+    url = "https://www.meteociel.fr/temps-reel/obs_villes.php?code2=63282001&jour2="+ day + "&mois2="+month+"&annee2="+year
+    return url 
